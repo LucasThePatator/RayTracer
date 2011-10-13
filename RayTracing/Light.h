650df2f3b1bd324241ddb7f9fd6deed14543115e
Lucas Marti <lmpatator@gmail.com>
2011-10-14 00:54:48 +0200

    Started adding non punctua lights

--- RayTracing/Light.h
+++ RayTracing/Light.h
@@ -2,20 +2,27 @@
 
 #include "commons.h"
 #include "Ray.h"
+#include "Primitive.h"
 
 class Light
 {
 public:
 	Light(void);
-	Light(Point3d &position, Color3d &color);
+	Light(Primitive &primitive, Material &Material);
 	~Light(void);
 
 	Color3d computeColor(const Ray& ray, double dist);
+	bool isPunctual() const {return punctual;}
+
+	const Primitive& getPrimitive() {return *primitive;}
 	const Vector3d& getPosition() const;
-	const Color3d& getColor() const;
+	const Color3d& getColor() const {return color;}
 
 private :
+
 	Point3d position;
 	Color3d color;
+	Primitive *primitive;
+	bool punctual;
 };
 
